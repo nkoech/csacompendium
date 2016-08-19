cfa85a343d69ff404465d9960ecd7beedb4aaa1a
Koech Nicholas <koechnicholas@gmail.com>
2016-08-20 00:22:32 +0300

    Added country serializer

---
+++ csacompendium/countries/api/serializers.py
@@ -0,0 +1,12 @@
+from rest_framework.serializers import ModelSerializer
+from csacompendium.countries.models import Country
+
+
+class CountrySerializer(ModelSerializer):
+    class Meta:
+        model = Country
+        fields = [
+            'id',
+            'country_code',
+            'country_name',
+        ]
