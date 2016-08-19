19d456e23096823c49f9b5cc5b98a1844af2d5b3
Koech Nicholas <koechnicholas@gmail.com>
2016-08-20 00:21:43 +0300

    Added country serializer

--- csacompendium/countries/api/views.py
+++ csacompendium/countries/api/views.py
@@ -1,7 +1,9 @@
 
 from rest_framework.generics import ListAPIView
 from csacompendium.countries.models import Country
+from .serializers import CountrySerializer
 
 
 class CountryListAPIView(ListAPIView):
     queryset = Country.objects.all()
+    serializer_class = CountrySerializer
