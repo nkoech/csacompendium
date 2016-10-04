e41007ae45be45fd0aeafa51bb424e69ee82059f
Koech Nicholas <koechnicholas@gmail.com>
2016-10-04 17:07:27 +0300

    Added measurement season urls

--- csacompendium/research/api/urls.py
+++ csacompendium/research/api/urls.py
@@ -1,11 +1,31 @@
 from django.conf.urls import url
 from .views import (
+    measurement_season_views,
     measurement_year_views,
     # soil_views,
 )
 
-# Measurement Year URLs
+# Measurement Season URLs
 urlpatterns = [
+    url(
+        r'^measurementseason/$',
+        measurement_season_views['MeasurementSeasonListAPIView'].as_view(),
+        name='measurement_season_list'
+    ),
+    url(
+        r'^measurementseason/create/$',
+        measurement_season_views['MeasurementSeasonCreateAPIView'].as_view(),
+        name='measurement_season_create'
+    ),
+    url(
+        r'^measurementseason/(?P<slug>[\w-]+)/$',
+        measurement_season_views['MeasurementSeasonDetailAPIView'].as_view(),
+        name='measurement_season_detail'
+    ),
+]
+
+# Measurement Year URLs
+urlpatterns += [
     url(
         r'^measurementyear/$',
         measurement_year_views['MeasurementYearListAPIView'].as_view(),
