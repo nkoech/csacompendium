2471b528c8ec380635215e79050cea1db7a175cf
Koech Nicholas <koechnicholas@gmail.com>
2016-10-04 14:13:27 +0300

    Added measurement year serializers

---
+++ csacompendium/research/api/serializers.py
@@ -0,0 +1,5 @@
+# from .soil.soilserializers import soil_serializers
+from .measurementyear.measurementyearserializers import measurement_year_serializers
+
+# soil_serializers = soil_serializers()
+measurement_year_serializers = measurement_year_serializers()
