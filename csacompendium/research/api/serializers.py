d4ad4938aae78f7a6ab0a6c8e0dae5851b080cf4
Koech Nicholas <koechnicholas@gmail.com>
2016-10-07 16:21:27 +0300

    Imported experiment duration serializers

--- csacompendium/research/api/serializers.py
+++ csacompendium/research/api/serializers.py
@@ -1,7 +1,9 @@
 # from .soil.soilserializers import soil_serializers
 from .measurementyear.measurementyearserializers import measurement_year_serializers
 from .measurementseason.measurementseasonserializers import measurement_season_serializers
+from .experimentduration.experimentdurationserializers import experiment_duration_serializers
 
 # soil_serializers = soil_serializers()
 measurement_year_serializers = measurement_year_serializers()
 measurement_season_serializers = measurement_season_serializers()
+experiment_duration_serializers = experiment_duration_serializers()
