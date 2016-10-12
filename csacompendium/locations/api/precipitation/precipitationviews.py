e9e69727d86132a38203992978a6a35fa021040b
Koech Nicholas <koechnicholas@gmail.com>
2016-10-12 12:21:22 +0300

    Added "DetailViewUpdateDelete"

--- csacompendium/locations/api/precipitation/precipitationviews.py
+++ csacompendium/locations/api/precipitation/precipitationviews.py
@@ -1,13 +1,9 @@
 from csacompendium.locations.models import Precipitation
 from csacompendium.utils.pagination import APILimitOffsetPagination
 from csacompendium.utils.permissions import IsOwnerOrReadOnly
+from csacompendium.utils.viewsutils import DetailViewUpdateDelete
 from rest_framework.filters import DjangoFilterBackend
-from rest_framework.generics import (
-    CreateAPIView,
-    ListAPIView,
-    RetrieveAPIView,
-)
-from rest_framework.mixins import DestroyModelMixin, UpdateModelMixin
+from rest_framework.generics import CreateAPIView, ListAPIView
 from rest_framework.permissions import (
     IsAuthenticated,
     IsAdminUser,
@@ -52,7 +48,7 @@ def precipitation_views():
         filter_class = PrecipitationListFilter
         pagination_class = APILimitOffsetPagination
 
-    class PrecipitationDetailAPIView(DestroyModelMixin, UpdateModelMixin, RetrieveAPIView):
+    class PrecipitationDetailAPIView(DetailViewUpdateDelete):
         """
         Updates a record.
         """
@@ -60,36 +56,6 @@ def precipitation_views():
         serializer_class = precipitation_serializers['PrecipitationDetailSerializer']
         permission_classes = [IsAuthenticated, IsAdminUser]
         lookup_field = 'pk'
-
-        def put(self, request, *args, **kwargs):
-            """
-            Update record
-            :param request: Client request
-            :param args: List arguments
-            :param kwargs: Keyworded arguments
-            :return: Updated record
-            :rtype: Object
-            """
-            return self.update(request, *args, **kwargs)
-
-        def delete(self, request, *args, **kwargs):
-            """
-            Delete record
-            :param request: Client request
-            :param args: List arguments
-            :param kwargs: Keyworded arguments
-            :return: Updated record
-            :rtype: Object
-            """
-            return self.destroy(request, *args, **kwargs)
-
-        def perform_update(self, serializer):
-            """
-            Update a field
-            :param serializer: Serializer object
-            :return:
-            """
-            serializer.save(modified_by=self.request.user)
 
     return {
         'PrecipitationListAPIView': PrecipitationListAPIView,
