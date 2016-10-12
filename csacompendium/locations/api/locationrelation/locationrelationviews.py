5c310288de11fee0050cb9a8233011fdad0619c6
Koech Nicholas <koechnicholas@gmail.com>
2016-10-12 12:16:15 +0300

    Added "DetailViewUpdateDelete"

--- csacompendium/locations/api/locationrelation/locationrelationviews.py
+++ csacompendium/locations/api/locationrelation/locationrelationviews.py
@@ -1,13 +1,9 @@
 from csacompendium.locations.models import LocationRelation
 from csacompendium.utils.pagination import APILimitOffsetPagination
 from csacompendium.utils.permissions import IsOwnerOrReadOnly
+from csacompendium.utils.viewsutils import DetailViewUpdateDelete
 from rest_framework.filters import DjangoFilterBackend
-from rest_framework.generics import (
-    CreateAPIView,
-    ListAPIView,
-    RetrieveAPIView,
-)
-from rest_framework.mixins import DestroyModelMixin, UpdateModelMixin
+from rest_framework.generics import CreateAPIView, ListAPIView
 from rest_framework.permissions import (
     IsAuthenticated,
     IsAdminUser,
@@ -51,7 +47,7 @@ def location_relation_views():
         filter_class = LocationRelationListFilter
         pagination_class = APILimitOffsetPagination
 
-    class LocationRelationDetailAPIView(DestroyModelMixin, UpdateModelMixin, RetrieveAPIView):
+    class LocationRelationDetailAPIView(DetailViewUpdateDelete):
         """
         Creates, deletes and updates a record.
         """
@@ -59,36 +55,6 @@ def location_relation_views():
         serializer_class = location_relation_serializers['LocationRelationDetailSerializer']
         permission_classes = [IsAuthenticated, IsAdminUser]
         lookup_field = 'pk'
-
-        def put(self, request, *args, **kwargs):
-            """
-            Update record
-            :param request: Client request
-            :param args: List arguments
-            :param kwargs: Keyworded arguments
-            :return: Updated record
-            :rtype: Object
-            """
-            return self.update(request, *args, **kwargs)
-
-        def delete(self, request, *args, **kwargs):
-            """
-            Delete record
-            :param request: Client request
-            :param args: List arguments
-            :param kwargs: Keyworded arguments
-            :return: Updated record
-            :rtype: Object
-            """
-            return self.destroy(request, *args, **kwargs)
-
-        def perform_update(self, serializer):
-            """
-            Update individual value
-            :param serializer: Serializer object
-            :return:
-            """
-            serializer.save(modified_by=self.request.user)
 
     return {
         'LocationRelationCreateAPIView': LocationRelationCreateAPIView,
