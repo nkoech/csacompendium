bc6bb71db2616cb5aa7ff3539932216d63a7a296
Koech Nicholas <koechnicholas@gmail.com>
2016-10-12 12:34:22 +0300

    Formatted imports

--- csacompendium/research/api/author/authorviews.py
+++ csacompendium/research/api/author/authorviews.py
@@ -1,17 +1,10 @@
 from csacompendium.research.models import Author
 from csacompendium.utils.pagination import APILimitOffsetPagination
 from csacompendium.utils.permissions import IsOwnerOrReadOnly
+from csacompendium.utils.viewsutils import DetailViewUpdateDelete
 from rest_framework.filters import DjangoFilterBackend
-from rest_framework.generics import (
-    CreateAPIView,
-    ListAPIView,
-    RetrieveAPIView,
-)
-from rest_framework.mixins import DestroyModelMixin, UpdateModelMixin
-from rest_framework.permissions import (
-    IsAuthenticated,
-    IsAdminUser,
-)
+from rest_framework.generics import CreateAPIView, ListAPIView
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from .filters import AuthorListFilter
 from csacompendium.research.api.author.authorserializers import author_serializers
 author_serializers = author_serializers()
@@ -51,7 +44,7 @@ def author_views():
         filter_class = AuthorListFilter
         pagination_class = APILimitOffsetPagination
 
-    class AuthorDetailAPIView(DestroyModelMixin, UpdateModelMixin, RetrieveAPIView):
+    class AuthorDetailAPIView(DetailViewUpdateDelete):
         """
         Updates a record.
         """
@@ -59,36 +52,6 @@ def author_views():
         serializer_class = author_serializers['AuthorDetailSerializer']
         permission_classes = [IsAuthenticated, IsAdminUser]
         lookup_field = 'slug'
-
-        def put(self, request, *args, **kwargs):
-            """
-            Update record
-            :param request: Client request
-            :param args: List arguments
-            :param kwargs: Keyworded arguments
-            :return: Updated record
-            :rtype: Object
-            """
-            return self.update(request, *args, **kwargs)
-
-        def delete(self, request, *args, **kwargs):
-            """
-            Delete record
-            :param request: Client request
-            :param args: List arguments
-            :param kwargs: Keyworded arguments
-            :return: Updated record
-            :rtype: Object
-            """
-            return self.destroy(request, *args, **kwargs)
-
-        def perform_update(self, serializer):
-            """
-            Update a field
-            :param serializer: Serializer object
-            :return:
-            """
-            serializer.save(modified_by=self.request.user)
 
     return {
         'AuthorListAPIView': AuthorListAPIView,
