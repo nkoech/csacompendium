e068ca1a932cfaef17d7aa48fad5115084794035
Koech Nicholas <koechnicholas@gmail.com>
2016-10-16 13:37:30 +0300

    Added research object serializer

--- csacompendium/research/api/serializers.py
+++ csacompendium/research/api/serializers.py
@@ -2,6 +2,7 @@
 from .measurementyear.measurementyearserializers import measurement_year_serializers
 from .measurementseason.measurementseasonserializers import measurement_season_serializers
 from .experimentduration.experimentdurationserializers import experiment_duration_serializers
+from .researchobject.researchobjectserializers import research_object_serializers
 from .experimentobject.experimentobjectserializers import experiment_object_serializers
 from .objectcategory.objectcategoryserializers import object_category_serializers
 
@@ -9,5 +10,6 @@ from .objectcategory.objectcategoryserializers import object_category_serializer
 measurement_year_serializers = measurement_year_serializers()
 measurement_season_serializers = measurement_season_serializers()
 experiment_duration_serializers = experiment_duration_serializers()
+research_object_serializers = research_object_serializers()
 experiment_object_serializers = experiment_object_serializers()
 object_category_serializers = object_category_serializers()
