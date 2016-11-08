67b64767121394072df90c5011a27291e80b6a63
Koech Nicholas <koechnicholas@gmail.com>
2016-11-08 13:30:21 +0300

    Registered research type in admin

--- csacompendium/research_type/admin.py
+++ csacompendium/research_type/admin.py
@@ -1,3 +1,78 @@
 from django.contrib import admin
 
+from django.contrib import admin
+
 # Register your models here.
+from .models import (
+    ExperimentRep,
+    NitrogenApplied,
+    ExperimentDetails,
+    ControlResearch,
+    TreatmentResearch,
+)
+
+
+class TreatmentResearchModelAdmin(admin.ModelAdmin):
+    """
+    Treatment research model admin settings
+    """
+    list_display = ['experimentdetails', 'experimentrep', 'nitrogenapplied', 'last_update', 'modified_by']
+    list_display_links = ['experimentdetails']
+    list_filter = ['experimentrep', 'nitrogenapplied', 'last_update', 'modified_by']
+
+    class Meta:
+        model = TreatmentResearch
+
+
+class ControlResearchModelAdmin(admin.ModelAdmin):
+    """
+    Control research model admin settings
+    """
+    list_display = ['experimentdetails', 'experimentrep', 'nitrogenapplied', 'last_update', 'modified_by']
+    list_display_links = ['experimentdetails']
+    list_filter = ['experimentrep', 'nitrogenapplied', 'last_update', 'modified_by']
+
+    class Meta:
+        model = ControlResearch
+
+
+class ExperimentDetailsModelAdmin(admin.ModelAdmin):
+    """
+    Experiment details model admin settings
+    """
+    list_display = ['exp_detail', 'last_update', 'modified_by']
+    list_display_links = ['exp_detail']
+    list_filter = ['exp_detail', 'last_update', 'modified_by']
+
+    class Meta:
+        model = ExperimentDetails
+
+
+class NitrogenAppliedModelAdmin(admin.ModelAdmin):
+    """
+    Nitrogen applied model admin settings
+    """
+    list_display = ['nitrogen_amount', 'amount_uom', 'last_update', 'modified_by']
+    list_display_links = ['nitrogen_amount']
+    list_filter = ['nitrogen_amount', 'amount_uom', 'last_update', 'modified_by']
+
+    class Meta:
+        model = NitrogenApplied
+
+
+class ExperimentRepModelAdmin(admin.ModelAdmin):
+    """
+    Experiment replication model admin settings
+    """
+    list_display = ['no_replication', 'last_update', 'modified_by']
+    list_display_links = ['no_replication']
+    list_filter = ['no_replication', 'last_update', 'modified_by']
+
+    class Meta:
+        model = ExperimentRep
+
+admin.site.register(TreatmentResearch, TreatmentResearchModelAdmin)
+admin.site.register(ControlResearch, ControlResearchModelAdmin)
+admin.site.register(ExperimentDetails, ExperimentDetailsModelAdmin)
+admin.site.register(NitrogenApplied, NitrogenAppliedModelAdmin)
+admin.site.register(ExperimentRep, ExperimentRepModelAdmin)
