f99ebe11708da05f6cd1565157e6ab8f9b4cc83a
Koech Nicholas <koechnicholas@gmail.com>
2016-11-08 10:37:07 +0300

    Added "TreatmentResearch" model and manager

--- csacompendium/research_type/models.py
+++ csacompendium/research_type/models.py
@@ -192,7 +192,7 @@ class ControlResearch(AuthUserDetail, CreateUpdateTime):
     content_type = models.ForeignKey(ContentType, on_delete=models.PROTECT, limit_choices_to=limit)
     object_id = models.PositiveIntegerField()
     content_object = GenericForeignKey('content_type', 'object_id')
-    csapractice = models.ForeignKey(CsaPractice, on_delete=models.PROTECT, verbose_name='CSA Practice')
+    # csapractice = models.ForeignKey(CsaPractice, on_delete=models.PROTECT, verbose_name='CSA Practice')
     experimentrep = models.ForeignKey(
         ExperimentRep, on_delete=models.PROTECT, blank=True, null=True, verbose_name='Experiment Replications'
     )
@@ -213,3 +213,71 @@ class ControlResearch(AuthUserDetail, CreateUpdateTime):
     class Meta:
         ordering = ['-time_created', '-last_update']
         verbose_name_plural = 'Control Research'
+
+
+class TreatmentResearchManager(models.Manager):
+    """
+    Treatment research model manager
+    """
+    def filter_by_instance(self, instance):
+        """
+        Query a related treatment research object/record from another model's object
+        :param instance: Object instance
+        :return: Query result from content type/model
+        :rtye: object/record
+        """
+        return model_instance_filter(instance, self, TreatmentResearchManager)
+
+    def filter_by_model_type(self, instance):
+        """
+        Query related objects/model type
+        :param instance: Object instance
+        :return: Matching object else none
+        :rtype: Object/record
+        """
+        obj_qs = model_foreign_key_qs(instance, self, TreatmentResearchManager)
+        if obj_qs.exists():
+            return model_type_filter(self, obj_qs, TreatmentResearchManager)
+
+    def create_by_model_type(self, model_type, pk, **kwargs):
+        """
+        Create object by model type
+        :param model_type: Content/model type
+        :param pk: Primary Key
+        :param kwargs: Fields to be created
+        :return: Data object
+        :rtype: Object
+        """
+        return create_model_type(self, model_type, pk, slugify=False, **kwargs)
+
+
+class TreatmentResearch(AuthUserDetail, CreateUpdateTime):
+    """
+    Creates treatment research entity.
+    """
+
+    limit = models.Q(app_label='research', model='research')
+    content_type = models.ForeignKey(ContentType, on_delete=models.PROTECT, limit_choices_to=limit)
+    object_id = models.PositiveIntegerField()
+    content_object = GenericForeignKey('content_type', 'object_id')
+    # csapractice = models.ForeignKey(CsaPractice, on_delete=models.PROTECT, verbose_name='CSA Practice')
+    experimentrep = models.ForeignKey(
+        ExperimentRep, on_delete=models.PROTECT, blank=True, null=True, verbose_name='Experiment Replications'
+    )
+    experimentdetails = models.ForeignKey(
+        ExperimentDetails, on_delete=models.PROTECT, blank=True, null=True, verbose_name='Experiment Details'
+    )
+    nitrogenapplied = models.ForeignKey(
+        NitrogenApplied, on_delete=models.PROTECT, blank=True, null=True, verbose_name='Nitrogen Applied'
+    )
+    objects = TreatmentResearchManager()
+
+    def __unicode__(self):
+        return str(self.experimentdetails)
+
+    def __str__(self):
+        return str(self.experimentdetails)
+
+    class Meta:
+        ordering = ['-time_created', '-last_update']
+        verbose_name_plural = 'Treatment Research'
