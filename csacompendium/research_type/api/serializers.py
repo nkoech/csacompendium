a29bb5f7a625951b59aa6defce9cae3a856d23ad
Koech Nicholas <koechnicholas@gmail.com>
2016-11-09 11:50:20 +0300

    Added "Nitrogen Applied" serializers

--- csacompendium/research_type/api/serializers.py
+++ csacompendium/research_type/api/serializers.py
@@ -1,4 +1,5 @@
 from .experimentrep.experimentrepserializers import experiment_rep_serializers
+from .nitrogenapplied.nitrogenappliedserializers import nitrogen_applied_serializers
 # from .controlresearch.controlresearchserializers import control_research_serializers
 # from .treatmentresearch.treatmentresearchserializers import treatment_research_serializers
 
@@ -6,3 +7,4 @@ from .experimentrep.experimentrepserializers import experiment_rep_serializers
 # treatment_research_serializers = treatment_research_serializers()
 # control_research_serializers = control_research_serializers()
 experiment_rep_serializers = experiment_rep_serializers()
+nitrogen_applied_serializers = nitrogen_applied_serializers()
