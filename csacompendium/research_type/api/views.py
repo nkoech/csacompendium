5ead3c2ce801daa53da7bd0a5c1dcb9da782fca4
Koech Nicholas <koechnicholas@gmail.com>
2016-11-09 10:27:08 +0300

    Added experiment replication views

---
+++ csacompendium/research_type/api/views.py
@@ -0,0 +1,8 @@
+# from .treatmentresearch.treatmentresearchviews import treatment_research_views
+# from .controlresearch.controlresearchviews import control_research_views
+from .experimentrep.experimentrepviews import experiment_rep_views
+
+
+# treatment_research_views = treatment_research_views()
+# control_research_views = control_research_views()
+experiment_rep_views = experiment_rep_views()
