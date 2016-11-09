ba1fb4b7aa3ca4d512af2e064db4235917516c3a
Koech Nicholas <koechnicholas@gmail.com>
2016-11-09 12:29:47 +0300

    Added "Nitrogen applied" urls

--- csacompendium/research_type/api/urls.py
+++ csacompendium/research_type/api/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import url
 from .views import (
     experiment_rep_views,
+    nitrogen_applied_views,
 )
 
 # Experiment replication URLs
@@ -19,5 +20,24 @@ urlpatterns = [
         r'^experimentreplication/(?P<pk>[\w-]+)/$',
         experiment_rep_views['ExperimentRepDetailAPIView'].as_view(),
         name='experiment_rep_detail'
+    ),
+]
+
+# Nitrogen Applied URLs
+urlpatterns += [
+    url(
+        r'^nitrogenapplied/$',
+        nitrogen_applied_views['NitrogenAppliedListAPIView'].as_view(),
+        name='nitrogen_applied_list'
+    ),
+    url(
+        r'^nitrogenapplied/create/$',
+        nitrogen_applied_views['NitrogenAppliedCreateAPIView'].as_view(),
+        name='nitrogen_applied_create'
+    ),
+    url(
+        r'^nitrogenapplied/(?P<pk>[\w-]+)/$',
+        nitrogen_applied_views['NitrogenAppliedDetailAPIView'].as_view(),
+        name='nitrogen_applied_detail'
     ),
 ]
