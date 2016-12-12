8f41d45dcef23b6072e6ce9c777426a957f61995
Koech Nicholas <koechnicholas@gmail.com>
2016-12-12 16:58:30 +0300

    Added "controlresearch" filter

--- csacompendium/research_type/api/controlresearch/filters.py
+++ csacompendium/research_type/api/controlresearch/filters.py
@@ -1,1 +1,22 @@
 
+from rest_framework.filters import (
+    FilterSet
+)
+from csacompendium.research_type.models import ControlResearch
+
+
+class ControlResearchListFilter(FilterSet):
+    """
+    Filter query list from control research database
+    """
+    class Meta:
+        model = ControlResearch
+        fields = {
+            'csapractice': ['exact'],
+            'experimentrep': ['exact'],
+            'experimentdetails': ['exact'],
+            'nitrogenapplied': ['exact'],
+            'object_id': ['exact'],
+            'content_type': ['exact'],
+        }
+        order_by = ['csapractice']
