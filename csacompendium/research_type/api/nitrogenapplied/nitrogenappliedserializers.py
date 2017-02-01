cbed0dce3b72b1c1d0a27d5374e33baa6327f7f0
Koech Nicholas <koechnicholas@gmail.com>
2017-02-01 14:41:18 +0300

    Uncommented serializers and views

--- csacompendium/research_type/api/nitrogenapplied/nitrogenappliedserializers.py
+++ csacompendium/research_type/api/nitrogenapplied/nitrogenappliedserializers.py
@@ -2,8 +2,8 @@ from rest_framework.serializers import (
     ModelSerializer,
     SerializerMethodField
 )
-# from csacompendium.research_type.api.controlresearch.controlresearchserializers import control_research_serializers
-# from csacompendium.research_type.api.treatmentresearch.treatmentresearchserializers import treatment_research_serializers
+from csacompendium.research_type.api.controlresearch.controlresearchserializers import control_research_serializers
+from csacompendium.research_type.api.treatmentresearch.treatmentresearchserializers import treatment_research_serializers
 from csacompendium.research_type.models import NitrogenApplied
 from csacompendium.utils.hyperlinkedidentity import hyperlinked_identity
 from csacompendium.utils.serializersutils import FieldMethodSerializer, get_related_content
@@ -34,8 +34,8 @@ def nitrogen_applied_serializers():
         """
         Serialize single record into an API. This is dependent on fields given.
         """
-        # control_research_serializers = control_research_serializers()
-        # treatment_research_serializers = treatment_research_serializers()
+        control_research_serializers = control_research_serializers()
+        treatment_research_serializers = treatment_research_serializers()
         user = SerializerMethodField()
         modified_by = SerializerMethodField()
         control_research = SerializerMethodField()
@@ -65,11 +65,11 @@ def nitrogen_applied_serializers():
             :rtype: Object/record
             """
             request = self.context['request']
-            # ControlResearchListSerializer = self.control_research_serializers['ControlResearchListSerializer']
-            # related_content = get_related_content(
-            #     obj, ControlResearchListSerializer, obj.control_research_relation, request
-            # )
-            # return related_content
+            ControlResearchListSerializer = self.control_research_serializers['ControlResearchListSerializer']
+            related_content = get_related_content(
+                obj, ControlResearchListSerializer, obj.control_research_relation, request
+            )
+            return related_content
 
         def get_treatment_research(self, obj):
             """
@@ -78,11 +78,11 @@ def nitrogen_applied_serializers():
             :rtype: Object/record
             """
             request = self.context['request']
-            # TreatmentResearchListSerializer = self.treatment_research_serializers['TreatmentResearchListSerializer']
-            # related_content = get_related_content(
-            #     obj, TreatmentResearchListSerializer, obj.treatment_research_relation, request
-            # )
-            # return related_content
+            TreatmentResearchListSerializer = self.treatment_research_serializers['TreatmentResearchListSerializer']
+            related_content = get_related_content(
+                obj, TreatmentResearchListSerializer, obj.treatment_research_relation, request
+            )
+            return related_content
 
     return {
         'NitrogenAppliedListSerializer': NitrogenAppliedListSerializer,
