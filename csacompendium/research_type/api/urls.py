159dd1d3950e70a106093ec9179074c6bdbd9526
Koech Nicholas <koechnicholas@gmail.com>
2017-02-12 02:52:55 +0300

    Added "Measurement Season" urls

--- csacompendium/research_type/api/urls.py
+++ csacompendium/research_type/api/urls.py
@@ -4,6 +4,8 @@ from .views import (
     nitrogen_applied_views,
     experiment_details_views,
     experiment_duration_views,
+    measurement_season_views,
+    # measurement_year_views,
     control_research_views,
     treatment_research_views,
 )
@@ -83,6 +85,44 @@ urlpatterns += [
         name='experiment_duration_detail'
     ),
 ]
+
+# Measurement Season URLs
+urlpatterns += [
+    url(
+        r'^measurementseason/$',
+        measurement_season_views['MeasurementSeasonListAPIView'].as_view(),
+        name='measurement_season_list'
+    ),
+    url(
+        r'^measurementseason/create/$',
+        measurement_season_views['MeasurementSeasonCreateAPIView'].as_view(),
+        name='measurement_season_create'
+    ),
+    url(
+        r'^measurementseason/(?P<slug>[\w-]+)/$',
+        measurement_season_views['MeasurementSeasonDetailAPIView'].as_view(),
+        name='measurement_season_detail'
+    ),
+]
+
+# # Measurement Year URLs
+# urlpatterns += [
+#     url(
+#         r'^measurementyear/$',
+#         measurement_year_views['MeasurementYearListAPIView'].as_view(),
+#         name='measurement_year_list'
+#     ),
+#     url(
+#         r'^measurementyear/create/$',
+#         measurement_year_views['MeasurementYearCreateAPIView'].as_view(),
+#         name='measurement_year_create'
+#     ),
+#     url(
+#         r'^measurementyear/(?P<pk>[\w-]+)/$',
+#         measurement_year_views['MeasurementYearDetailAPIView'].as_view(),
+#         name='measurement_year_detail'
+#     ),
+# ]
 
 # Control research URLs
 urlpatterns += [
