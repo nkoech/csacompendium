c882eb3490aff915eac7a7d75a0d0527bf8fa157
Koech Nicholas <koechnicholas@gmail.com>
2017-02-13 02:58:27 +0300

    Added "Species" filters

--- csacompendium/research_type/api/species/filters.py
+++ csacompendium/research_type/api/species/filters.py
@@ -1,14 +1,14 @@
-# from rest_framework.filters import (
-#     FilterSet
-# )
-# from csacompendium.research.models import Species
-#
-#
-# class SpeciesListFilter(FilterSet):
-#     """
-#     Filter query list from precipitation database
-#     """
-#     class Meta:
-#         model = Species
-#         fields = {'species': ['iexact', 'icontains'], }
-#         order_by = ['species']
+from rest_framework.filters import (
+    FilterSet
+)
+from csacompendium.research_type.models import Species
+
+
+class SpeciesListFilter(FilterSet):
+    """
+    Filter query list from species database
+    """
+    class Meta:
+        model = Species
+        fields = {'species': ['iexact', 'icontains'], }
+        order_by = ['species']
--- csacompendium/research_type/api/species/speciesviews.py
+++ csacompendium/research_type/api/species/speciesviews.py
@@ -1,52 +1,52 @@
-# from csacompendium.research.models import Species
-# from csacompendium.utils.pagination import APILimitOffsetPagination
-# from csacompendium.utils.permissions import IsOwnerOrReadOnly
-# from csacompendium.utils.viewsutils import DetailViewUpdateDelete, CreateAPIViewHook
-# from rest_framework.filters import DjangoFilterBackend
-# from rest_framework.generics import ListAPIView
-# from rest_framework.permissions import IsAuthenticated, IsAdminUser
-# from .filters import SpeciesListFilter
-# from csacompendium.research.api.species.speciesserializers import species_serializers
-#
-#
-# def species_views():
-#     """
-#     Species views
-#     :return: All species views
-#     :rtype: Object
-#     """
-#     species_serializer = species_serializers()
-#
-#     class SpeciesCreateAPIView(CreateAPIViewHook):
-#         """
-#         Creates a single record.
-#         """
-#         queryset = Species.objects.all()
-#         serializer_class = species_serializer['SpeciesDetailSerializer']
-#         permission_classes = [IsAuthenticated]
-#
-#     class SpeciesListAPIView(ListAPIView):
-#         """
-#         API list view. Gets all records API.
-#         """
-#         queryset = Species.objects.all()
-#         serializer_class = species_serializer['SpeciesListSerializer']
-#         filter_backends = (DjangoFilterBackend,)
-#         filter_class = SpeciesListFilter
-#         pagination_class = APILimitOffsetPagination
-#
-#     class SpeciesDetailAPIView(DetailViewUpdateDelete):
-#         """
-#         Updates a record
-#         """
-#         queryset = Species.objects.all()
-#         serializer_class = species_serializer['SpeciesDetailSerializer']
-#         permission_classes = [IsAuthenticated, IsAdminUser]
-#         lookup_field = 'slug'
-#
-#     return {
-#         'SpeciesListAPIView': SpeciesListAPIView,
-#         'SpeciesDetailAPIView': SpeciesDetailAPIView,
-#         'SpeciesCreateAPIView': SpeciesCreateAPIView
-#     }
-#
+from csacompendium.research_type.models import Species
+from csacompendium.utils.pagination import APILimitOffsetPagination
+from csacompendium.utils.permissions import IsOwnerOrReadOnly
+from csacompendium.utils.viewsutils import DetailViewUpdateDelete, CreateAPIViewHook
+from rest_framework.filters import DjangoFilterBackend
+from rest_framework.generics import ListAPIView
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
+from .filters import SpeciesListFilter
+from  csacompendium.research_type.api.species.speciesserializers import species_serializers
+
+
+def author_views():
+    """
+    Author views
+    :return: All author views
+    :rtype: Object
+    """
+    author_serializer = author_serializers()
+
+    class AuthorCreateAPIView(CreateAPIViewHook):
+        """
+        Creates a single record.
+        """
+        queryset = Author.objects.all()
+        serializer_class = author_serializer['AuthorDetailSerializer']
+        permission_classes = [IsAuthenticated]
+
+    class AuthorListAPIView(ListAPIView):
+        """
+        API list view. Gets all records API.
+        """
+        queryset = Author.objects.all()
+        serializer_class = author_serializer['AuthorListSerializer']
+        filter_backends = (DjangoFilterBackend,)
+        filter_class = AuthorListFilter
+        pagination_class = APILimitOffsetPagination
+
+    class AuthorDetailAPIView(DetailViewUpdateDelete):
+        """
+        Updates a record.
+        """
+        queryset = Author.objects.all()
+        serializer_class = author_serializer['AuthorDetailSerializer']
+        permission_classes = [IsAuthenticated, IsAdminUser]
+        lookup_field = 'slug'
+
+    return {
+        'AuthorListAPIView': AuthorListAPIView,
+        'AuthorDetailAPIView': AuthorDetailAPIView,
+        'AuthorCreateAPIView': AuthorCreateAPIView
+    }
+
