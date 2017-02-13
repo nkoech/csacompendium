27baa965292bf16d70c4243111a4dc26c4616d5c
Koech Nicholas <koechnicholas@gmail.com>
2017-02-13 12:42:38 +0300

    Added "research species" serializers

--- csacompendium/research_type/api/researchspecies/researchspeciesviews.py
+++ csacompendium/research_type/api/researchspecies/researchspeciesviews.py
@@ -1,58 +1,58 @@
-from csacompendium.research_type.models import ResearchAuthor
+from csacompendium.research_type.models import ResearchSpecies
 from csacompendium.utils.pagination import APILimitOffsetPagination
 from csacompendium.utils.permissions import IsOwnerOrReadOnly
 from csacompendium.utils.viewsutils import DetailViewUpdateDelete, get_http_request
 from rest_framework.filters import DjangoFilterBackend
 from rest_framework.generics import CreateAPIView, ListAPIView
 from rest_framework.permissions import IsAuthenticated, IsAdminUser
-from .filters import ResearchAuthorListFilter
-from csacompendium.research_type.api.serializers import research_author_serializers
+from .filters import ResearchSpeciesListFilter
+from csacompendium.research_type.api.serializers import research_species_serializers
 
 
-def research_author_views():
+def research_species_views():
     """
-    Research author views
-    :return: All research author views
+    Research species views
+    :return: All research species views
     :rtype: Object
     """
-    class ResearchAuthorCreateAPIView(CreateAPIView):
+    class ResearchSpeciesCreateAPIView(CreateAPIView):
         """
         Creates a single record.
         """
-        queryset = ResearchAuthor.objects.all()
+        queryset = ResearchSpecies.objects.all()
         permission_classes = [IsAuthenticated]
 
         def get_serializer_class(self):
             """
             Gets serializer class
-            :return: Research author object
+            :return: Research species object
             :rtype: Object
             """
             model_type, url_parameter, user = get_http_request(self.request, slug=False)
-            create_research_author_serializer = research_author_serializers['create_research_author_serializer']
-            return create_research_author_serializer(model_type, url_parameter, user)
+            create_research_species_serializer = research_species_serializers['create_research_species_serializer']
+            return create_research_species_serializer(model_type, url_parameter, user)
 
-    class ResearchAuthorListAPIView(ListAPIView):
+    class ResearchSpeciesListAPIView(ListAPIView):
         """
         API list view. Gets all records API.
         """
-        queryset = ResearchAuthor.objects.all()
-        serializer_class = research_author_serializers['ResearchAuthorListSerializer']
+        queryset = ResearchSpecies.objects.all()
+        serializer_class = research_species_serializers['ResearchSpeciesListSerializer']
         filter_backends = (DjangoFilterBackend,)
-        filter_class = ResearchAuthorListFilter
+        filter_class = ResearchSpeciesListFilter
         pagination_class = APILimitOffsetPagination
 
-    class ResearchAuthorDetailAPIView(DetailViewUpdateDelete):
+    class ResearchSpeciesDetailAPIView(DetailViewUpdateDelete):
         """
         Creates, deletes and updates a record.
         """
-        queryset = ResearchAuthor.objects.all()
-        serializer_class = research_author_serializers['ResearchAuthorDetailSerializer']
+        queryset = ResearchSpecies.objects.all()
+        serializer_class = research_species_serializers['ResearchSpeciesDetailSerializer']
         permission_classes = [IsAuthenticated, IsAdminUser]
         lookup_field = 'pk'
 
     return {
-        'ResearchAuthorCreateAPIView': ResearchAuthorCreateAPIView,
-        'ResearchAuthorListAPIView': ResearchAuthorListAPIView,
-        'ResearchAuthorDetailAPIView': ResearchAuthorDetailAPIView
+        'ResearchSpeciesCreateAPIView': ResearchSpeciesCreateAPIView,
+        'ResearchSpeciesListAPIView': ResearchSpeciesListAPIView,
+        'ResearchSpeciesDetailAPIView': ResearchSpeciesDetailAPIView
     }
