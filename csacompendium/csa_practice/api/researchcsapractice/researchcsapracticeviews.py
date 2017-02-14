759b3f94ae2e7d749dee685a9b77f4ed0d5e586a
Koech Nicholas <koechnicholas@gmail.com>
2017-02-14 15:08:10 +0300

    Added "Research CSA practice" views

--- csacompendium/csa_practice/api/researchcsapractice/researchcsapracticeviews.py
+++ csacompendium/csa_practice/api/researchcsapractice/researchcsapracticeviews.py
@@ -6,56 +6,55 @@ from rest_framework.filters import DjangoFilterBackend
 from rest_framework.generics import CreateAPIView, ListAPIView
 from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from .filters import ResearchCsaPracticeListFilter
-from csacompendium.indicators.api.serializers import research_outcome_indicator_serializers
-from csacompendium.csa_practice.api.serializers import
+from csacompendium.csa_practice.api.serializers import research_csa_practice_serializers
 
 
 def research_csa_practice_views():
     """
-    Research outcome indicator views
-    :return: All research outcome indicator views
+    Research CSA practice views
+    :return: All research CSA practice views
     :rtype: Object
     """
-    class ResearchOutcomeIndicatorCreateAPIView(CreateAPIView):
+    class ResearchCsaPracticeCreateAPIView(CreateAPIView):
         """
         Creates a single record.
         """
-        queryset = ResearchOutcomeIndicator.objects.all()
+        queryset = ResearchCsaPractice.objects.all()
         permission_classes = [IsAuthenticated]
 
         def get_serializer_class(self):
             """
             Gets serializer class
-            :return: Research outcome indicator object
+            :return: Research CSA practice object
             :rtype: Object
             """
             model_type, url_parameter, user = get_http_request(self.request, slug=False)
-            create_research_outcome_indicator_serializer = research_outcome_indicator_serializers[
-                'create_research_outcome_indicator_serializer'
+            create_research_csa_practice_serializer = research_csa_practice_serializers[
+                'create_research_csa_practice_serializer'
             ]
-            return create_research_outcome_indicator_serializer(model_type, url_parameter, user)
+            return create_research_csa_practice_serializer(model_type, url_parameter, user)
 
-    class ResearchOutcomeIndicatorListAPIView(ListAPIView):
+    class ResearchCsaPracticeListAPIView(ListAPIView):
         """
         API list view. Gets all records API.
         """
-        queryset = ResearchOutcomeIndicator.objects.all()
-        serializer_class = research_outcome_indicator_serializers['ResearchOutcomeIndicatorListSerializer']
+        queryset = ResearchCsaPractice.objects.all()
+        serializer_class = research_csa_practice_serializers['ResearchCsaPracticeListSerializer']
         filter_backends = (DjangoFilterBackend,)
-        filter_class = ResearchOutcomeIndicatorListFilter
+        filter_class = ResearchCsaPracticeListFilter
         pagination_class = APILimitOffsetPagination
 
-    class ResearchOutcomeIndicatorDetailAPIView(DetailViewUpdateDelete):
+    class ResearchCsaPracticeDetailAPIView(DetailViewUpdateDelete):
         """
         Creates, deletes and updates a record.
         """
-        queryset = ResearchOutcomeIndicator.objects.all()
-        serializer_class = research_outcome_indicator_serializers['ResearchOutcomeIndicatorDetailSerializer']
+        queryset = ResearchCsaPractice.objects.all()
+        serializer_class = research_csa_practice_serializers['ResearchCsaPracticeDetailSerializer']
         permission_classes = [IsAuthenticated, IsAdminUser]
         lookup_field = 'pk'
 
     return {
-        'ResearchOutcomeIndicatorCreateAPIView': ResearchOutcomeIndicatorCreateAPIView,
-        'ResearchOutcomeIndicatorListAPIView': ResearchOutcomeIndicatorListAPIView,
-        'ResearchOutcomeIndicatorDetailAPIView': ResearchOutcomeIndicatorDetailAPIView
+        'ResearchCsaPracticeCreateAPIView': ResearchCsaPracticeCreateAPIView,
+        'ResearchCsaPracticeListAPIView': ResearchCsaPracticeListAPIView,
+        'ResearchCsaPracticeDetailAPIView': ResearchCsaPracticeDetailAPIView
     }
--- csacompendium/csa_practice/api/views.py
+++ csacompendium/csa_practice/api/views.py
@@ -1,9 +1,11 @@
 from .csapractice.csapracticeviews import csa_practice_views
+from .researchcsapractice.researchcsapracticeviews import research_csa_practice_views
 from .csatheme.csathemeviews import csa_theme_views
 from .practicelevel.practicelevelviews import practice_level_views
 from .practicetype.practicetypeviews import practice_type_views
 
 csa_practice_views = csa_practice_views()
+research_csa_practice_views = research_csa_practice_views()
 csa_theme_views = csa_theme_views()
 practice_level_views = practice_level_views()
 practice_type_views = practice_type_views()
