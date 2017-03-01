a508506cd236f1317cd09db875c4e08b19d4fede
Koech Nicholas <koechnicholas@gmail.com>
2017-03-01 13:45:09 +0300

    Added field method serializer and base serializers to "Treatment research" serializer

--- csacompendium/research/api/treatmentresearch/treatmentresearchserializers.py
+++ csacompendium/research/api/treatmentresearch/treatmentresearchserializers.py
@@ -28,6 +28,11 @@ from rest_framework.serializers import (
     ValidationError,
 )
 
+research_author_serializers = research_author_serializers()
+research_species_serializers = research_species_serializers()
+research_outcome_indicator_serializers = research_outcome_indicator_serializers()
+research_csa_practice_serializers = research_csa_practice_serializers()
+research_experiment_unit_serializers = research_experiment_unit_serializers()
 
 def treatment_research_serializers():
     """
@@ -35,6 +40,162 @@ def treatment_research_serializers():
     :return: All treatment research serializers
     :rtype: Object
     """
+
+    class TreatmentResearchBaseSerializer(ModelSerializer):
+        """
+        Base serializer for DRY implementation.
+        """
+        experiment_replications_url = SerializerMethodField()
+        experiment_details_url = SerializerMethodField()
+        nitrogen_applied_url = SerializerMethodField()
+        experiment_duration_url = SerializerMethodField()
+        measurement_year_url = SerializerMethodField()
+
+        class Meta:
+            model = TreatmentResearch
+            fields = [
+                'id', 'experiment_replications_url', 'experiment_details_url', 'nitrogen_applied_url',
+                'experiment_duration_url', 'measurement_year_url', 'mean_outcome', 'std_outcome',
+                'outcome_uom',
+            ]
+
+    class TreatmentResearchManyToManyBaseSerializer(ModelSerializer):
+        """
+        Base serializer for DRY implementation.
+        """
+        content_type_url = SerializerMethodField()
+        authors = SerializerMethodField()
+        species = SerializerMethodField()
+        outcome_indicator = SerializerMethodField()
+        csa_practice = SerializerMethodField()
+        experiment_unit = SerializerMethodField()
+
+        class Meta:
+            model = TreatmentResearch
+            fields = [
+                'content_type_url',
+                'authors',
+                'species',
+                'outcome_indicator',
+                'csa_practice',
+                'experiment_unit',
+            ]
+
+    class TreatmentResearchFieldMethodSerializer:
+        """
+        Serialize an object based on a provided field
+        """
+        def get_experiment_details_url(self, obj):
+            """
+            Get related content type/object url
+            :param obj: Current record object
+            :return: URL to related object
+            :rtype: String
+            """
+            return get_related_content_url(ExperimentRep, obj.experimentrep.id)
+
+        def get_experiment_replications_url(self, obj):
+            """
+            Get related content type/object url
+            :param obj: Current record object
+            :return: URL to related object
+            :rtype: String
+            """
+            return get_related_content_url(ExperimentDetails, obj.experimentdetails.id)
+
+        def get_nitrogen_applied_url(self, obj):
+            """
+            Get related content type/object url
+            :param obj: Current record object
+            :return: URL to related object
+            :rtype: String
+            """
+            return get_related_content_url(NitrogenApplied, obj.nitrogenapplied.id)
+
+        def get_experiment_duration_url(self, obj):
+            """
+            Get related content type/object url
+            :param obj: Current record object
+            :return: URL to related object
+            :rtype: String
+            """
+            return get_related_content_url(ExperimentDuration, obj.experimentduration.id)
+
+        def get_measurement_year_url(self, obj):
+            """
+            Get related content type/object url
+            :param obj: Current record object
+            :return: URL to related object
+            :rtype: String
+            """
+            return get_related_content_url(MeasurementYear, obj.measurementyear.id)
+
+        def get_authors(self, obj):
+            """
+            :param obj: Current record object
+            :return: :return: Related author details
+            :rtype: Object/record
+            """
+            request = self.context['request']
+            ResearchAuthorSerializer = research_author_serializers['ResearchAuthorSerializer']
+            related_content = get_related_content(obj, ResearchAuthorSerializer, obj.research_author, request)
+            return related_content
+
+        def get_species(self, obj):
+            """
+            :param obj: Current record object
+            :return: Related species details
+            :rtype: Object/record
+            """
+            request = self.context['request']
+            ResearchSpeciesSerializer = research_species_serializers['ResearchSpeciesSerializer']
+            related_content = get_related_content(obj, ResearchSpeciesSerializer, obj.research_species, request)
+            return related_content
+
+        def get_outcome_indicator(self, obj):
+            """
+            :param obj: Current record object
+            :return: Related outcome indicator details
+            :rtype: Object/record
+            """
+            request = self.context['request']
+            ResearchOutcomeIndicatorSerializer = research_outcome_indicator_serializers[
+                'ResearchOutcomeIndicatorSerializer'
+            ]
+            related_content = get_related_content(
+                obj, ResearchOutcomeIndicatorSerializer, obj.research_outcome_indicator, request
+            )
+            return related_content
+
+        def get_csa_practice(self, obj):
+            """
+            :param obj: Current record object
+            :return: Related CSA practice details
+            :rtype: Object/record
+            """
+            request = self.context['request']
+            ResearchCsaPracticeSerializer = research_csa_practice_serializers[
+                'ResearchCsaPracticeSerializer'
+            ]
+            related_content = get_related_content(
+                obj, ResearchCsaPracticeSerializer, obj.research_csa_practice, request
+            )
+            return related_content
+
+        def get_experiment_unit(self, obj):
+            """
+            :param obj: Current record object
+            :return: Related experiment unit details
+            :rtype: Object/record
+            """
+            request = self.context['request']
+            ResearchExperimentUnitSerializer = research_experiment_unit_serializers[
+                'ResearchExperimentUnitSerializer'
+            ]
+            related_content = get_related_content(
+                obj, ResearchExperimentUnitSerializer, obj.research_experiment_unit, request
+            )
+            return related_content
 
     def create_treatment_research_serializer(model_type=None, request_key=None, user=None):
         """
@@ -103,7 +264,10 @@ def treatment_research_serializers():
 
         return TreatmentResearchCreateSerializer
 
-    class TreatmentResearchListSerializer(ModelSerializer):
+    class TreatmentResearchListSerializer(
+        TreatmentResearchBaseSerializer, TreatmentResearchManyToManyBaseSerializer,
+        FieldMethodSerializer, TreatmentResearchFieldMethodSerializer
+    ):
         """
         Serialize all records in given fields into an API
         """
@@ -111,163 +275,29 @@ def treatment_research_serializers():
 
         class Meta:
             model = TreatmentResearch
-            fields = ['id', 'mean_outcome', 'std_outcome', 'outcome_uom', 'url', ]
+            fields = TreatmentResearchBaseSerializer.Meta.fields + ['url', ] + \
+                     TreatmentResearchManyToManyBaseSerializer.Meta.fields
 
-    class TreatmentResearchDetailSerializer(ModelSerializer, FieldMethodSerializer):
+    class TreatmentResearchDetailSerializer(
+        TreatmentResearchBaseSerializer, TreatmentResearchManyToManyBaseSerializer,
+        FieldMethodSerializer, TreatmentResearchFieldMethodSerializer
+    ):
         """
         Serialize single record into an API. This is dependent on fields given.
         """
-        experiment_replications_url = SerializerMethodField()
-        experiment_details_url = SerializerMethodField()
-        nitrogen_applied_url = SerializerMethodField()
-        experiment_duration_url = SerializerMethodField()
-        measurement_year_url = SerializerMethodField()
-        research_author_serializers = research_author_serializers()
-        research_species_serializers = research_species_serializers()
-        research_outcome_indicator_serializers = research_outcome_indicator_serializers()
-        research_csa_practice_serializers = research_csa_practice_serializers()
-        research_experiment_unit_serializers = research_experiment_unit_serializers()
         user = SerializerMethodField()
         modified_by = SerializerMethodField()
-        content_type_url = SerializerMethodField()
-        authors = SerializerMethodField()
-        species = SerializerMethodField()
-        outcome_indicator = SerializerMethodField()
-        csa_practice = SerializerMethodField()
-        experiment_unit = SerializerMethodField()
 
         class Meta:
             common_fields = [
+                'user',
                 'modified_by',
                 'last_update',
                 'time_created',
-                'content_type_url',
-                'authors',
-                'species',
-                'outcome_indicator',
-                'csa_practice',
-                'experiment_unit',
-            ]
+            ] + TreatmentResearchManyToManyBaseSerializer.Meta.fields
             model = TreatmentResearch
-            fields = [
-                'id', 'experiment_replications_url', 'experiment_details_url', 'nitrogen_applied_url',
-                'experiment_duration_url', 'measurement_year_url', 'mean_outcome', 'std_outcome',
-                'outcome_uom', 'user',
-            ] + common_fields
+            fields = TreatmentResearchBaseSerializer.Meta.fields + common_fields
             read_only_fields = ['id', ] + common_fields
-
-        def get_experiment_details_url(self, obj):
-            """
-            Get related content type/object url
-            :param obj: Current record object
-            :return: URL to related object
-            :rtype: String
-            """
-            return get_related_content_url(ExperimentRep, obj.experimentrep.id)
-
-        def get_experiment_replications_url(self, obj):
-            """
-            Get related content type/object url
-            :param obj: Current record object
-            :return: URL to related object
-            :rtype: String
-            """
-            return get_related_content_url(ExperimentDetails, obj.experimentdetails.id)
-
-        def get_nitrogen_applied_url(self, obj):
-            """
-            Get related content type/object url
-            :param obj: Current record object
-            :return: URL to related object
-            :rtype: String
-            """
-            return get_related_content_url(NitrogenApplied, obj.nitrogenapplied.id)
-
-        def get_experiment_duration_url(self, obj):
-            """
-            Get related content type/object url
-            :param obj: Current record object
-            :return: URL to related object
-            :rtype: String
-            """
-            return get_related_content_url(ExperimentDuration, obj.experimentduration.id)
-
-        def get_measurement_year_url(self, obj):
-            """
-            Get related content type/object url
-            :param obj: Current record object
-            :return: URL to related object
-            :rtype: String
-            """
-            return get_related_content_url(MeasurementYear, obj.measurementyear.id)
-
-        def get_authors(self, obj):
-            """
-            :param obj: Current record object
-            :return: :return: Related author details
-            :rtype: Object/record
-            """
-            request = self.context['request']
-            ResearchAuthorSerializer = self.research_author_serializers['ResearchAuthorSerializer']
-            related_content = get_related_content(obj, ResearchAuthorSerializer, obj.research_author, request)
-            return related_content
-
-        def get_species(self, obj):
-            """
-            :param obj: Current record object
-            :return: Related species details
-            :rtype: Object/record
-            """
-            request = self.context['request']
-            ResearchSpeciesSerializer = self.research_species_serializers['ResearchSpeciesSerializer']
-            related_content = get_related_content(obj, ResearchSpeciesSerializer, obj.research_species, request)
-            return related_content
-
-        def get_outcome_indicator(self, obj):
-            """
-            :param obj: Current record object
-            :return: Related outcome indicator details
-            :rtype: Object/record
-            """
-            request = self.context['request']
-            ResearchOutcomeIndicatorSerializer = self.research_outcome_indicator_serializers[
-                'ResearchOutcomeIndicatorSerializer'
-            ]
-            related_content = get_related_content(
-                obj, ResearchOutcomeIndicatorSerializer, obj.research_outcome_indicator, request
-            )
-            return related_content
-
-        def get_csa_practice(self, obj):
-            """
-            :param obj: Current record object
-            :return: Related CSA practice details
-            :rtype: Object/record
-            """
-            request = self.context['request']
-            ResearchCsaPracticeSerializer = self.research_csa_practice_serializers[
-                'ResearchCsaPracticeSerializer'
-            ]
-            related_content = get_related_content(
-                obj, ResearchCsaPracticeSerializer, obj.research_csa_practice, request
-            )
-            return related_content
-
-        def get_experiment_unit(self, obj):
-            """
-            :param obj: Current record object
-            :return: Related experiment unit details
-            :rtype: Object/record
-            """
-            request = self.context['request']
-            ResearchExperimentUnitSerializer = self.research_experiment_unit_serializers[
-                'ResearchExperimentUnitSerializer'
-            ]
-            related_content = get_related_content(
-                obj, ResearchExperimentUnitSerializer, obj.research_experiment_unit, request
-            )
-            return related_content
-
     return {
         'create_treatment_research_serializer': create_treatment_research_serializer,
         'TreatmentResearchListSerializer': TreatmentResearchListSerializer,
