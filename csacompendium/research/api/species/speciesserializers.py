0e2568dbced262d253ed973f19c4259c9e12f007
Koech Nicholas <koechnicholas@gmail.com>
2017-03-01 11:40:55 +0300

    Added field method serialiser to "Species" serializer

--- csacompendium/research/api/species/speciesserializers.py
+++ csacompendium/research/api/species/speciesserializers.py
@@ -7,6 +7,8 @@ from csacompendium.research.models import Species
 from csacompendium.utils.hyperlinkedidentity import hyperlinked_identity
 from csacompendium.utils.serializersutils import FieldMethodSerializer, get_related_content
 
+research_species_serializers = research_species_serializers()
+
 
 def species_serializers():
     """
@@ -22,24 +24,45 @@ def species_serializers():
         class Meta:
             model = Species
             fields = [
+                'id',
                 'species',
             ]
 
-    class SpeciesListSerializer(SpeciesBaseSerializer):
+    class SpeciesFieldMethodSerializer:
+        """
+        Serialize an object based on a provided field
+        """
+        def get_research_relation(self, obj):
+            """
+            Gets control/treatment research record
+            :param obj: Current record object
+            :return: Related research object/record
+            :rtype: Object/record
+            """
+            request = self.context['request']
+            ResearchSpeciesContentTypeSerializer = research_species_serializers[
+                'ResearchSpeciesContentTypeSerializer'
+            ]
+            related_content = get_related_content(
+                obj, ResearchSpeciesContentTypeSerializer, obj.research_species_relation, request
+            )
+            return related_content
+
+    class SpeciesListSerializer(SpeciesBaseSerializer, SpeciesFieldMethodSerializer):
         """
         Serialize all records in given fields into an API
         """
+        research_relation = SerializerMethodField()
         url = hyperlinked_identity('research_api:species_detail', 'slug')
 
         class Meta:
             model = Species
-            fields = SpeciesBaseSerializer.Meta.fields + ['url', ]
+            fields = SpeciesBaseSerializer.Meta.fields + ['url', 'research_relation', ]
 
-    class SpeciesDetailSerializer(SpeciesBaseSerializer, FieldMethodSerializer):
+    class SpeciesDetailSerializer(SpeciesBaseSerializer, FieldMethodSerializer, SpeciesFieldMethodSerializer):
         """
         Serialize single record into an API. This is dependent on fields given.
         """
-        research_species_serializers = research_species_serializers()
         user = SerializerMethodField()
         modified_by = SerializerMethodField()
         research_relation = SerializerMethodField()
@@ -53,25 +76,8 @@ def species_serializers():
                 'research_relation',
             ]
             model = Species
-            fields = ['id', ] + SpeciesBaseSerializer.Meta.fields + common_fields
+            fields = SpeciesBaseSerializer.Meta.fields + common_fields
             read_only_fields = ['id', ] + common_fields
-
-        def get_research_relation(self, obj):
-            """
-            Gets control/treatment research record
-            :param obj: Current record object
-            :return: Related research object/record
-            :rtype: Object/record
-            """
-            request = self.context['request']
-            ResearchSpeciesContentTypeSerializer = self.research_species_serializers[
-                'ResearchSpeciesContentTypeSerializer'
-            ]
-            related_content = get_related_content(
-                obj, ResearchSpeciesContentTypeSerializer, obj.research_species_relation, request
-            )
-            return related_content
-
     return {
         'SpeciesListSerializer': SpeciesListSerializer,
         'SpeciesDetailSerializer': SpeciesDetailSerializer
