35000137238dc836831e8ca57492dcb0c682ed70
Koech Nicholas <koechnicholas@gmail.com>
2017-03-02 19:21:37 +0300

    Added serializer method to "Countries" serializer

--- csacompendium/countries/api/serializers.py
+++ csacompendium/countries/api/serializers.py
@@ -8,50 +8,36 @@ from csacompendium.utils.hyperlinkedidentity import hyperlinked_identity
 from csacompendium.utils.serializersutils import FieldMethodSerializer, get_related_content
 
 
-class CountryListSerializer(ModelSerializer):
+class CountryBaseSerializer(ModelSerializer):
     """
-    Serialize all records in given fields into an API
+    Base serializer for DRY implementation.
     """
-    url = hyperlinked_identity('country_api:detail', 'slug')
-
     class Meta:
         model = Country
         fields = [
-            'country_name',
+            'id',
             'country_code',
-            'url',
+            'country_name',
         ]
 
 
-class CountryDetailSerializer(ModelSerializer, FieldMethodSerializer):
+class CountryRelationBaseSerializer(ModelSerializer):
     """
-    Serialize single record into an API. This is dependent on fields given.
+    Base serializer for DRY implementation.
     """
-    user = SerializerMethodField()
-    modified_by = SerializerMethodField()
     locations = SerializerMethodField()
 
     class Meta:
         model = Country
         fields = [
-            'id',
-            'country_code',
-            'country_name',
-            'user',
-            'modified_by',
-            'last_update',
-            'time_created',
-            'locations',
-        ]
-        read_only_fields = [
-            'id',
-            'user',
-            'modified_by',
-            'last_update',
-            'time_created',
             'locations',
         ]
 
+
+class CountryFieldMethodSerializer:
+    """
+    Serialize an object based on a provided field
+    """
     def get_locations(self, obj):
         """
         :param obj: Current record object
@@ -62,3 +48,41 @@ class CountryDetailSerializer(ModelSerializer, FieldMethodSerializer):
         LocationListSerializer = location_serializers['LocationListSerializer']
         related_content = get_related_content(obj, LocationListSerializer, obj.locations, request)
         return related_content
+
+
+class CountryListSerializer(
+    CountryBaseSerializer,
+    CountryRelationBaseSerializer,
+    CountryFieldMethodSerializer
+):
+    """
+    Serialize all records in given fields into an API
+    """
+    url = hyperlinked_identity('country_api:detail', 'slug')
+
+    class Meta:
+        model = Country
+        fields = CountryBaseSerializer.Meta.fields + ['url', ] + \
+                 CountryRelationBaseSerializer.Meta.fields
+
+
+class CountryDetailSerializer(
+    CountryBaseSerializer, CountryRelationBaseSerializer,
+    FieldMethodSerializer, CountryFieldMethodSerializer
+):
+    """
+    Serialize single record into an API. This is dependent on fields given.
+    """
+    user = SerializerMethodField()
+    modified_by = SerializerMethodField()
+
+    class Meta:
+        common_fields = [
+            'user',
+            'modified_by',
+            'last_update',
+            'time_created',
+        ] + CountryRelationBaseSerializer.Meta.fields
+        model = Country
+        fields = CountryBaseSerializer.Meta.fields + common_fields
+        read_only_fields = ['id', ] + common_fields
