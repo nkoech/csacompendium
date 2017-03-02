44cf6382c6651f0bfd63d85ca500a3b9bf139831
Koech Nicholas <koechnicholas@gmail.com>
2017-03-02 17:28:25 +0300

    Added serializer method to "CSA theme" serializer

--- csacompendium/csa_practice/api/csatheme/csathemeserializers.py
+++ csacompendium/csa_practice/api/csatheme/csathemeserializers.py
@@ -7,6 +7,8 @@ from csacompendium.csa_practice.models import CsaTheme
 from csacompendium.utils.hyperlinkedidentity import hyperlinked_identity
 from csacompendium.utils.serializersutils import FieldMethodSerializer, get_related_content
 
+csa_practice_serializers = csa_practice_serializers()
+
 
 def csa_theme_serializers():
     """
@@ -15,43 +17,33 @@ def csa_theme_serializers():
     :rtype: Object
     """
 
-    class CsaThemeListSerializer(ModelSerializer):
+    class CsaThemeBaseSerializer(ModelSerializer):
         """
-        Serialize all records in given fields into an API
+        Base serializer for DRY implementation.
         """
-        url = hyperlinked_identity('csa_practice_api:csa_theme_detail', 'slug')
-
         class Meta:
-            model = CsaTheme
-            fields = [
-                'csa_theme',
-                'url',
-            ]
-
-    class CsaThemeDetailSerializer(ModelSerializer, FieldMethodSerializer):
-        """
-        Serialize single record into an API. This is dependent on fields given.
-        """
-        csa_practice_serializers = csa_practice_serializers()
-        user = SerializerMethodField()
-        modified_by = SerializerMethodField()
-        csa_practices = SerializerMethodField()
-
-        class Meta:
-            common_fields = [
-                'user',
-                'modified_by',
-                'last_update',
-                'time_created',
-                'csa_practices',
-            ]
             model = CsaTheme
             fields = [
                 'id',
                 'csa_theme',
-            ] + common_fields
-            read_only_fields = ['id', ] + common_fields
+            ]
 
+    class CsaThemeRelationBaseSerializer(ModelSerializer):
+        """
+        Base serializer for DRY implementation.
+        """
+        csa_practices = SerializerMethodField()
+
+        class Meta:
+            model = CsaTheme
+            fields = [
+                'csa_practices',
+            ]
+
+    class CsaThemeFieldMethodSerializer:
+        """
+        Serialize an object based on a provided field
+        """
         def get_csa_practices(self, obj):
             """
             :param obj: Current record object
@@ -59,10 +51,45 @@ def csa_theme_serializers():
             :rtype: Object/record
             """
             request = self.context['request']
-            CsaPracticeListSerializer = self.csa_practice_serializers['CsaPracticeListSerializer']
+            CsaPracticeListSerializer = csa_practice_serializers['CsaPracticeListSerializer']
             related_content = get_related_content(obj, CsaPracticeListSerializer, obj.csa_practice_relation, request)
             return related_content
 
+    class CsaThemeListSerializer(
+        CsaThemeBaseSerializer,
+        CsaThemeRelationBaseSerializer,
+        CsaThemeFieldMethodSerializer
+    ):
+        """
+        Serialize all records in given fields into an API
+        """
+        url = hyperlinked_identity('csa_practice_api:csa_theme_detail', 'slug')
+
+        class Meta:
+            model = CsaTheme
+            fields = CsaThemeBaseSerializer.Meta.fields + ['url', ] + \
+                     CsaThemeRelationBaseSerializer.Meta.fields
+
+    class CsaThemeDetailSerializer(
+        CsaThemeBaseSerializer, CsaThemeRelationBaseSerializer,
+        FieldMethodSerializer, CsaThemeFieldMethodSerializer
+    ):
+        """
+        Serialize single record into an API. This is dependent on fields given.
+        """
+        user = SerializerMethodField()
+        modified_by = SerializerMethodField()
+
+        class Meta:
+            common_fields = [
+                'user',
+                'modified_by',
+                'last_update',
+                'time_created',
+            ] + CsaThemeRelationBaseSerializer.Meta.fields
+            model = CsaTheme
+            fields = CsaThemeBaseSerializer.Meta.fields + common_fields
+            read_only_fields = ['id', ] + common_fields
     return {
         'CsaThemeListSerializer': CsaThemeListSerializer,
         'CsaThemeDetailSerializer': CsaThemeDetailSerializer
