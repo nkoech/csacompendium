54207ccf87791c837a82c04b6ebf6a86aa5f5fa1
Koech Nicholas <koechnicholas@gmail.com>
2017-03-02 17:45:59 +0300

    Added serializer method to "Practice level" serializer

--- csacompendium/csa_practice/api/practicelevel/practicelevelserializers.py
+++ csacompendium/csa_practice/api/practicelevel/practicelevelserializers.py
@@ -7,6 +7,7 @@ from csacompendium.csa_practice.models import PracticeLevel
 from csacompendium.utils.hyperlinkedidentity import hyperlinked_identity
 from csacompendium.utils.serializersutils import FieldMethodSerializer, get_related_content
 
+csa_practice_serializers = csa_practice_serializers()
 
 def practice_level_serializers():
     """
@@ -15,43 +16,33 @@ def practice_level_serializers():
     :rtype: Object
     """
 
-    class PracticeLevelListSerializer(ModelSerializer):
+    class PracticeLevelBaseSerializer(ModelSerializer):
         """
-        Serialize all records in given fields into an API
+        Base serializer for DRY implementation.
         """
-        url = hyperlinked_identity('csa_practice_api:practice_level_detail', 'slug')
-
         class Meta:
-            model = PracticeLevel
-            fields = [
-                'practice_level',
-                'url',
-            ]
-
-    class PracticeLevelDetailSerializer(ModelSerializer, FieldMethodSerializer):
-        """
-        Serialize single record into an API. This is dependent on fields given.
-        """
-        csa_practice_serializers = csa_practice_serializers()
-        user = SerializerMethodField()
-        modified_by = SerializerMethodField()
-        csa_practices = SerializerMethodField()
-
-        class Meta:
-            common_fields = [
-                'user',
-                'modified_by',
-                'last_update',
-                'time_created',
-                'csa_practices',
-            ]
             model = PracticeLevel
             fields = [
                 'id',
                 'practice_level',
-            ] + common_fields
-            read_only_fields = ['id', ] + common_fields
+            ]
 
+    class PracticeLevelRelationBaseSerializer(ModelSerializer):
+        """
+        Base serializer for DRY implementation.
+        """
+        csa_practices = SerializerMethodField()
+
+        class Meta:
+            model = PracticeLevel
+            fields = [
+                'csa_practices',
+            ]
+
+    class PracticeLevelFieldMethodSerializer:
+        """
+        Serialize an object based on a provided field
+        """
         def get_csa_practices(self, obj):
             """
             :param obj: Current record object
@@ -59,10 +50,45 @@ def practice_level_serializers():
             :rtype: Object/record
             """
             request = self.context['request']
-            CsaPracticeListSerializer = self.csa_practice_serializers['CsaPracticeListSerializer']
+            CsaPracticeListSerializer = csa_practice_serializers['CsaPracticeListSerializer']
             related_content = get_related_content(obj, CsaPracticeListSerializer, obj.csa_practice_relation, request)
             return related_content
 
+    class PracticeLevelListSerializer(
+        PracticeLevelBaseSerializer,
+        PracticeLevelRelationBaseSerializer,
+        PracticeLevelFieldMethodSerializer
+    ):
+        """
+        Serialize all records in given fields into an API
+        """
+        url = hyperlinked_identity('csa_practice_api:practice_level_detail', 'slug')
+
+        class Meta:
+            model = PracticeLevel
+            fields = PracticeLevelBaseSerializer.Meta.fields + ['url', ] + \
+                     PracticeLevelRelationBaseSerializer.Meta.fields
+
+    class PracticeLevelDetailSerializer(
+        PracticeLevelBaseSerializer, PracticeLevelRelationBaseSerializer,
+        FieldMethodSerializer, PracticeLevelFieldMethodSerializer
+    ):
+        """
+        Serialize single record into an API. This is dependent on fields given.
+        """
+        user = SerializerMethodField()
+        modified_by = SerializerMethodField()
+
+        class Meta:
+            common_fields = [
+                'user',
+                'modified_by',
+                'last_update',
+                'time_created',
+            ] + PracticeLevelRelationBaseSerializer.Meta.fields
+            model = PracticeLevel
+            fields = PracticeLevelBaseSerializer.Meta.fields + common_fields
+            read_only_fields = ['id', ] + common_fields
     return {
         'PracticeLevelListSerializer': PracticeLevelListSerializer,
         'PracticeLevelDetailSerializer': PracticeLevelDetailSerializer
