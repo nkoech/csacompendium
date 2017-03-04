fc6d13276a729e7358f3ab942901fe6d3df18f8d
Koech Nicholas <koechnicholas@gmail.com>
2017-03-04 23:52:24 +0300

    Added field method serializer to "Subpillar" serializer

--- csacompendium/indicators/api/subpillar/subpillarserializers.py
+++ csacompendium/indicators/api/subpillar/subpillarserializers.py
@@ -7,6 +7,8 @@ from csacompendium.indicators.models import Subpillar
 from csacompendium.utils.hyperlinkedidentity import hyperlinked_identity
 from csacompendium.utils.serializersutils import FieldMethodSerializer, get_related_content
 
+indicator_serializers = indicator_serializers()
+
 
 def subpillar_serializers():
     """
@@ -15,43 +17,34 @@ def subpillar_serializers():
     :rtype: Object
     """
 
-    class SubpillarListSerializer(ModelSerializer):
+    class SubpillarBaseSerializer(ModelSerializer):
         """
-        Serialize all records in given fields into an API
+        Base serializer for DRY implementation.
         """
-        url = hyperlinked_identity('indicator_outcome_api:subpillar_detail', 'slug')
 
         class Meta:
-            model = Subpillar
-            fields = [
-                'subpillar',
-                'url',
-            ]
-
-    class SubpillarDetailSerializer(ModelSerializer, FieldMethodSerializer):
-        """
-        Serialize single record into an API. This is dependent on fields given.
-        """
-        indicator_serializers = indicator_serializers()
-        user = SerializerMethodField()
-        modified_by = SerializerMethodField()
-        indicators = SerializerMethodField()
-
-        class Meta:
-            common_fields = [
-                'user',
-                'modified_by',
-                'last_update',
-                'time_created',
-                'indicators',
-            ]
             model = Subpillar
             fields = [
                 'id',
                 'subpillar',
-            ] + common_fields
-            read_only_fields = ['id', ] + common_fields
+            ]
 
+    class SubpillarRelationBaseSerializer(ModelSerializer):
+        """
+        Base serializer for DRY implementation.
+        """
+        indicators = SerializerMethodField()
+
+        class Meta:
+            model = Subpillar
+            fields = [
+                'indicators',
+            ]
+
+    class SubpillarFieldMethodSerializer:
+        """
+        Serialize an object based on a provided field
+        """
         def get_indicators(self, obj):
             """
             :param obj: Current record object
@@ -59,12 +52,46 @@ def subpillar_serializers():
             :rtype: Object/record
             """
             request = self.context['request']
-            IndicatorListSerializer = self.indicator_serializers['IndicatorListSerializer']
+            IndicatorListSerializer = indicator_serializers['IndicatorListSerializer']
             related_content = get_related_content(
                 obj, IndicatorListSerializer, obj.indicator_relation, request
             )
             return related_content
 
+    class SubpillarListSerializer(
+        SubpillarBaseSerializer,
+        SubpillarRelationBaseSerializer,
+        SubpillarFieldMethodSerializer
+    ):
+        """
+        Serialize all records in given fields into an API
+        """
+        url = hyperlinked_identity('indicator_outcome_api:subpillar_detail', 'slug')
+
+        class Meta:
+            model = Subpillar
+            fields = SubpillarBaseSerializer.Meta.fields + ['url', ] + \
+                     SubpillarRelationBaseSerializer.Meta.fields
+
+    class SubpillarDetailSerializer(
+        SubpillarBaseSerializer, SubpillarRelationBaseSerializer,
+        FieldMethodSerializer, SubpillarFieldMethodSerializer):
+        """
+        Serialize single record into an API. This is dependent on fields given.
+        """
+        user = SerializerMethodField()
+        modified_by = SerializerMethodField()
+
+        class Meta:
+            common_fields = [
+                'user',
+                'modified_by',
+                'last_update',
+                'time_created',
+            ] + SubpillarRelationBaseSerializer.Meta.fields
+            model = Subpillar
+            fields = SubpillarBaseSerializer.Meta.fields + common_fields
+            read_only_fields = ['id', ] + common_fields
     return {
         'SubpillarListSerializer': SubpillarListSerializer,
         'SubpillarDetailSerializer': SubpillarDetailSerializer
