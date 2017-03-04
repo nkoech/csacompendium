3443c49a85592a3631a8a117ef40ff872cf7f50e
Koech Nicholas <koechnicholas@gmail.com>
2017-03-05 01:04:33 +0300

    Added field method serializer to "Location" serializer

--- csacompendium/locations/api/location/locationserializers.py
+++ csacompendium/locations/api/location/locationserializers.py
@@ -17,6 +17,8 @@ from rest_framework.serializers import (
     ValidationError,
 )
 
+location_relation_serializers = location_relation_serializers()
+
 
 def location_serializers():
     """
@@ -32,84 +34,17 @@ def location_serializers():
         class Meta:
             model = Location
             fields = [
+                'id',
                 'location_name',
                 'latitude',
                 'longitude',
                 'elevation',
             ]
 
-    def create_location_serializer(model_type=None, slug=None, user=None):
+    class LocationRelationBaseSerializer(ModelSerializer):
         """
-        Creates a model serializer
-        :param model_type: Model
-        :param slug: Hyphened slug
-        :param user: Record owner
-        :return: Serializer class
-        :rtype: Object
+        Base serializer for DRY implementation.
         """
-
-        class LocationCreateSerializer(LocationBaseSerializer, CreateSerializerUtil):
-            """
-            Create a record
-            """
-            class Meta:
-                model = Location
-                fields = ['id', ] + LocationBaseSerializer.Meta.fields + \
-                         ['last_update', 'time_created', ]
-
-            def __init__(self, *args, **kwargs):
-                super(LocationCreateSerializer, self).__init__(*args, **kwargs)
-                self.model_type = model_type
-                self.key = slug
-                self.user = user
-                self.slugify = True
-                self.auth_user = self.get_authenticated_user(self.user)
-
-            def create(self, validated_data):
-                """
-                Created record from validated data
-                :param validated_data: Validated data
-                :return: Location object
-                :rtype: Object
-                """
-                location_name = validated_data.get('location_name')
-                latitude = validated_data.get('latitude')
-                longitude = validated_data.get('longitude')
-                elevation = validated_data.get('elevation')
-                location = Location.objects.create_by_model_type(
-                    self.model_type,
-                    self.key,
-                    location_name=location_name,
-                    latitude=latitude,
-                    longitude=longitude,
-                    elevation=elevation,
-                    user=self.auth_user,
-                    modified_by=self.auth_user
-                )
-                if location:
-                    return location
-                else:
-                    raise ValidationError({"non_field_errors": ["This is not a valid content type"]})
-
-        return LocationCreateSerializer
-
-    class LocationListSerializer(LocationBaseSerializer):
-        """
-        Serialize all records in given fields into an API
-        """
-        url = hyperlinked_identity('location_api:location_detail', 'slug')
-
-        class Meta:
-            model = Location
-            fields = LocationBaseSerializer.Meta.fields + ['url', ]
-
-    class LocationDetailSerializer(LocationBaseSerializer, FieldMethodSerializer):
-        """
-        Serialize single record into an API. This is dependent on fields given.
-        """
-        location_relation_serializers = location_relation_serializers()
-        user = SerializerMethodField()
-        modified_by = SerializerMethodField()
         content_type_url = SerializerMethodField()
         relation_details = SerializerMethodField()
         soils = SerializerMethodField()
@@ -117,20 +52,19 @@ def location_serializers():
         treatment_research = SerializerMethodField()
 
         class Meta:
-            common_fields = [
-                'modified_by',
-                'last_update',
-                'time_created',
+            model = Location
+            fields = [
                 'content_type_url',
                 'relation_details',
                 'soils',
                 'control_research',
                 'treatment_research',
             ]
-            model = Location
-            fields = ['id', ] + LocationBaseSerializer.Meta.fields + ['user', ] + common_fields
-            read_only_fields = ['id', ] + common_fields
 
+    class LocationFieldMethodSerializer:
+        """
+        Serialize an object based on a provided field
+        """
         def get_relation_details(self, obj):
             """
             Get related object type data
@@ -139,7 +73,7 @@ def location_serializers():
             :rtype: Object/record
             """
             request = self.context['request']
-            LocationRelationContentTypeSerializer = self.location_relation_serializers[
+            LocationRelationContentTypeSerializer = location_relation_serializers[
                 'LocationRelationContentTypeSerializer'
             ]
             related_content = get_related_content(
@@ -185,6 +119,94 @@ def location_serializers():
             )
             return related_content
 
+    def create_location_serializer(model_type=None, slug=None, user=None):
+        """
+        Creates a model serializer
+        :param model_type: Model
+        :param slug: Hyphened slug
+        :param user: Record owner
+        :return: Serializer class
+        :rtype: Object
+        """
+
+        class LocationCreateSerializer(LocationBaseSerializer, CreateSerializerUtil):
+            """
+            Create a record
+            """
+            class Meta:
+                model = Location
+                fields = LocationBaseSerializer.Meta.fields + ['last_update', 'time_created', ]
+
+            def __init__(self, *args, **kwargs):
+                super(LocationCreateSerializer, self).__init__(*args, **kwargs)
+                self.model_type = model_type
+                self.key = slug
+                self.user = user
+                self.slugify = True
+                self.auth_user = self.get_authenticated_user(self.user)
+
+            def create(self, validated_data):
+                """
+                Created record from validated data
+                :param validated_data: Validated data
+                :return: Location object
+                :rtype: Object
+                """
+                location_name = validated_data.get('location_name')
+                latitude = validated_data.get('latitude')
+                longitude = validated_data.get('longitude')
+                elevation = validated_data.get('elevation')
+                location = Location.objects.create_by_model_type(
+                    self.model_type,
+                    self.key,
+                    location_name=location_name,
+                    latitude=latitude,
+                    longitude=longitude,
+                    elevation=elevation,
+                    user=self.auth_user,
+                    modified_by=self.auth_user
+                )
+                if location:
+                    return location
+                else:
+                    raise ValidationError({"non_field_errors": ["This is not a valid content type"]})
+
+        return LocationCreateSerializer
+
+    class LocationListSerializer(
+        LocationBaseSerializer, LocationRelationBaseSerializer,
+        FieldMethodSerializer, LocationFieldMethodSerializer
+    ):
+        """
+        Serialize all records in given fields into an API
+        """
+        url = hyperlinked_identity('location_api:location_detail', 'slug')
+
+        class Meta:
+            model = Location
+            fields = LocationBaseSerializer.Meta.fields + ['url', ] + \
+                     LocationRelationBaseSerializer.Meta.fields
+
+    class LocationDetailSerializer(
+        LocationBaseSerializer, LocationRelationBaseSerializer,
+        FieldMethodSerializer, LocationFieldMethodSerializer
+    ):
+        """
+        Serialize single record into an API. This is dependent on fields given.
+        """
+        user = SerializerMethodField()
+        modified_by = SerializerMethodField()
+
+        class Meta:
+            common_fields = [
+                'user',
+                'modified_by',
+                'last_update',
+                'time_created',
+            ] + LocationRelationBaseSerializer.Meta.fields
+            model = Location
+            fields = LocationBaseSerializer.Meta.fields + common_fields
+            read_only_fields = ['id', ] + common_fields
     return {
         'create_location_serializer': create_location_serializer,
         'LocationListSerializer': LocationListSerializer,
