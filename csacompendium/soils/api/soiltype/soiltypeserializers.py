453ae79dc49e9979dd766ed4b8978496f5ed9ba8
Koech Nicholas <koechnicholas@gmail.com>
2017-03-05 03:17:54 +0300

    Added field method serializer to "Soil type" serializer

--- csacompendium/soils/api/soiltype/soiltypeserializers.py
+++ csacompendium/soils/api/soiltype/soiltypeserializers.py
@@ -7,6 +7,8 @@ from csacompendium.soils.models import SoilType
 from csacompendium.utils.hyperlinkedidentity import hyperlinked_identity
 from csacompendium.utils.serializersutils import FieldMethodSerializer, get_related_content
 
+soil_serializers = soil_serializers()
+
 
 def soil_type_serializers():
     """
@@ -15,45 +17,35 @@ def soil_type_serializers():
     :rtype: Object
     """
 
-    class SoilTypeListSerializer(ModelSerializer):
+    class SoilTypeBaseSerializer(ModelSerializer):
         """
-        Serialize all records in given fields into an API
+        Base serializer for DRY implementation.
         """
-        url = hyperlinked_identity('soil_api:soil_type_detail', 'slug')
 
         class Meta:
-            model = SoilType
-            fields = [
-                'soil_type',
-                'classification',
-                'url',
-            ]
-
-    class SoilTypeDetailSerializer(ModelSerializer, FieldMethodSerializer):
-        """
-        Serialize single record into an API. This is dependent on fields given.
-        """
-        soil_serializers = soil_serializers()
-        user = SerializerMethodField()
-        modified_by = SerializerMethodField()
-        soil_properties = SerializerMethodField()
-
-        class Meta:
-            common_fields = [
-                'user',
-                'modified_by',
-                'last_update',
-                'time_created',
-                'soil_properties',
-            ]
             model = SoilType
             fields = [
                 'id',
                 'soil_type',
                 'classification',
-            ] + common_fields
-            read_only_fields = ['id', ] + common_fields
+            ]
 
+    class SoilTypeRelationBaseSerializer(ModelSerializer):
+        """
+        Base serializer for DRY implementation.
+        """
+        soil_properties = SerializerMethodField()
+
+        class Meta:
+            model = SoilType
+            fields = [
+                'soil_properties',
+            ]
+
+    class SoilTypeFieldMethodSerializer:
+        """
+        Serialize an object based on a provided field
+        """
         def get_soil_properties(self, obj):
             """
             :param obj: Current record object
@@ -61,10 +53,45 @@ def soil_type_serializers():
             :rtype: Object/record
             """
             request = self.context['request']
-            SoilListSerializer = self.soil_serializers['SoilListSerializer']
+            SoilListSerializer = soil_serializers['SoilListSerializer']
             related_content = get_related_content(obj, SoilListSerializer, obj.soil_relation, request)
             return related_content
 
+    class SoilTypeListSerializer(
+        SoilTypeBaseSerializer,
+        SoilTypeRelationBaseSerializer,
+        SoilTypeFieldMethodSerializer
+    ):
+        """
+        Serialize all records in given fields into an API
+        """
+        url = hyperlinked_identity('soil_api:soil_type_detail', 'slug')
+
+        class Meta:
+            model = SoilType
+            fields = SoilTypeBaseSerializer.Meta.fields + ['url', ] + \
+                     SoilTypeRelationBaseSerializer.Meta.fields
+
+    class SoilTypeDetailSerializer(
+        SoilTypeBaseSerializer, SoilTypeRelationBaseSerializer,
+        FieldMethodSerializer, SoilTypeFieldMethodSerializer
+    ):
+        """
+        Serialize single record into an API. This is dependent on fields given.
+        """
+        user = SerializerMethodField()
+        modified_by = SerializerMethodField()
+
+        class Meta:
+            common_fields = [
+                'user',
+                'modified_by',
+                'last_update',
+                'time_created',
+            ] + SoilTypeRelationBaseSerializer.Meta.fields
+            model = SoilType
+            fields = SoilTypeBaseSerializer.Meta.fields + common_fields
+            read_only_fields = ['id', ] + common_fields
     return {
         'SoilTypeListSerializer': SoilTypeListSerializer,
         'SoilTypeDetailSerializer': SoilTypeDetailSerializer
