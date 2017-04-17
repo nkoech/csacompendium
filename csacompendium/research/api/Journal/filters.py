0af8d30910fad831ac3d06e431a881155d82e088
Koech Nicholas <koechnicholas@gmail.com>
2017-04-17 23:52:47 +0300

    Added "Journal" filters

--- csacompendium/research/api/Journal/filters.py
+++ csacompendium/research/api/Journal/filters.py
@@ -1,1 +1,17 @@
+from rest_framework.filters import (
+    FilterSet
+)
+from csacompendium.research.models import Journal
 
+
+class JournalListFilter(FilterSet):
+    """
+    Filter query list from journal database table
+    """
+    class Meta:
+        model = Journal
+        fields = {
+            'journal_tag': ['iexact', 'icontains'],
+            'publication_year': ['exact', 'gte', 'lte'],
+        }
+        order_by = ['publication_year']
