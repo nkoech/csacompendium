f0e3f318940a0b0880ebd5547dd720f4868e9d02
Koech Nicholas <koechnicholas@gmail.com>
2017-04-21 07:39:23 +0300

    Added "MeasurementYear" filterset

---
+++ csacompendium/research/api/measurementyear/filters.py
@@ -0,0 +1,16 @@
+from rest_framework.filters import (
+    FilterSet
+)
+from csacompendium.research.models import MeasurementYear
+
+
+class MeasurementYearListFilter(FilterSet):
+    """
+    Filter query list from measurement year database table
+    """
+    class Meta:
+        model = MeasurementYear
+        fields = {
+            'measurement_year': ['iexact', 'icontains'],
+        }
+        order_by = ['measurement_year']
