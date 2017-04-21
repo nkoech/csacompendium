eef803a47c5af2a4419e5106eefbf10931a56534
Koech Nicholas <koechnicholas@gmail.com>
2017-04-21 18:59:59 +0300

    Added "NitrogenApplied" views and filterset

---
+++ csacompendium/research/api/nitrogenapplied/filters.py
@@ -0,0 +1,18 @@
+from rest_framework.filters import (
+    FilterSet
+)
+from csacompendium.research.models import NitrogenApplied
+
+
+class NitrogenAppliedListFilter(FilterSet):
+    """
+    Filter query list from nitrogen applied database table
+    """
+    class Meta:
+        model = NitrogenApplied
+        fields = {
+            'nitrogen_amount': ['exact', 'gte', 'lte'],
+            'amount_uom': ['iexact', 'icontains'],
+            'nitrogen_source': ['iexact', 'icontains'],
+        }
+        order_by = ['nitrogen_amount']
