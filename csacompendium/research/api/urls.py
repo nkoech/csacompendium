57f3f83d07b8a755167479cedff5e3ca527ddfdc
Koech Nicholas <koechnicholas@gmail.com>
2017-04-21 07:29:27 +0300

    Added "ResearchMeasurementYear" urls

--- csacompendium/research/api/urls.py
+++ csacompendium/research/api/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import url
 from .views import (
     nitrogen_applied_views,
+    research_measurement_year_views,
     research_diversity_views,
     diversity_views,
     research_experiment_description_views,
@@ -33,6 +34,25 @@ urlpatterns = [
         r'^nitrogenapplied/(?P<pk>[\w-]+)/$',
         nitrogen_applied_views['NitrogenAppliedDetailAPIView'].as_view(),
         name='nitrogen_applied_detail'
+    ),
+]
+
+# Research measurement year URLs
+urlpatterns += [
+    url(
+        r'^researchmeasurementyear/$',
+        research_measurement_year_views['ResearchMeasurementYearListAPIView'].as_view(),
+        name='research_measurement_year_list'
+    ),
+    url(
+        r'^researchmeasurementyear/create/$',
+        research_measurement_year_views['ResearchMeasurementYearCreateAPIView'].as_view(),
+        name='research_measurement_year_create'
+    ),
+    url(
+        r'^researchmeasurementyear/(?P<pk>[\w-]+)/$',
+        research_measurement_year_views['ResearchMeasurementYearDetailAPIView'].as_view(),
+        name='research_measurement_year_detail'
     ),
 ]
 
