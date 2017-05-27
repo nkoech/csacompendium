ff594b5dae9a7183a6b27d659d3da941549d71c0
Koech Nicholas <koechnicholas@gmail.com>
2017-05-27 21:27:06 +0300

    Added 'core' module

--- csacompendium/frontend/src/app/app.module.js
+++ csacompendium/frontend/src/app/app.module.js
@@ -1,25 +1,8 @@
-// require('bootstrap-css-only');
-// require('font-awesome');
-// import '../../node_modules/bootstrap-css-only/css/bootstrap.min.css'
-// import '../../node_modules/font-awesome/css/font-awesome.min.css'
 import '../assets/css/main.css';
 import '../test_users.html';
 import '../test_index.html';
-require('angular');
-require('angular-ui-bootstrap');
-import { RandomGenerator } from './random-generator';
-const outputParagraph = $('#outputParagraph');
+import angular from 'angular';
+import appCore from './core/core.module';
 
-const outputRandomInt = () => {
-    outputParagraph.html(RandomGenerator.randomInteger());
-};
+angular.module('app', [appCore.name]);
 
-const outputRandomRange = () => {
-    outputParagraph.html(RandomGenerator.randomRange(1, 500));
-};
-
-const buttonRndInt = $('#randomInt');
-const buttonRndRange = $('#randomRange');
-
-buttonRndInt.click(outputRandomInt);
-buttonRndRange.click(outputRandomRange);
---
+++ csacompendium/frontend/src/app/core/core.module.js
@@ -0,0 +1,5 @@
+import angular from 'angular';
+import uiBootstrap from 'angular-ui-bootstrap';
+
+const appCore = angular.module('app.core', [uiBootstrap]);
+export default appCore;
